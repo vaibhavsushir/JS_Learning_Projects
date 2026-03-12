e7ba9c52acb030edcfe4424066b26f532d57d0cc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-12 07:26:17 +0530

    Add files via upload

---
+++ 7GuessMovie.js
@@ -0,0 +1,11 @@
+let fav="krish";
+let guess=prompt("Guess Fav Movie");
+
+while((fav != guess) && (guess != "quit")){
+    console.log("Wrong");
+    guess=prompt("Wrong,Please Guess Fav Movie");
+    
+}
+if(guess==fav){
+    console.log("Congrats");
+}
