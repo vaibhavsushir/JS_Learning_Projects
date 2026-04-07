cd5123e3a359e7a4aa245d3023f6f83ad1bb443d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-07 08:51:05 +0530

    Update 07GuessMovie.js

--- 07GuessMovie.js
+++ 07GuessMovie.js
@@ -2,8 +2,7 @@ let fav="krish";
 let guess=prompt("Guess Fav Movie");
 while((fav != guess) && (guess != "quit")){
     console.log("Wrong");
-    guess=prompt("Wrong,Please Guess Fav Movie");
-    
+    guess=prompt("Wrong,Please Guess Fav Movie");   
 }
 if(guess==fav){
     console.log("Congrats");
