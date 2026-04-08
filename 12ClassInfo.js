b24fe7ff2877dae6001fa6a49149598019d8a500
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-08 08:15:23 +0530

    Add files via upload

---
+++ 12ClassInfo.js
@@ -0,0 +1,18 @@
+let classarr=[
+    {
+        name:"Sanika",
+        mark:50,
+        gender:"Female"
+    },
+    {
+        name:"ravi",
+        mark:50,
+        gender:"male"
+    },
+    {
+        name:"karan",
+        mark:50,
+        gender:"male"
+    }
+    
+]
