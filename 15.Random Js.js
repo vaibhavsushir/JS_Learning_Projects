1f5d3a665dccc9b8239563e13c46f37386d9bf71
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-12 10:35:35 +0530

    Add files via upload

---
+++ 15.Random Js.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click",function(){
+    let h3 = document.querySelector("h3");
+    let random = GetRandomColour();
+    h3.innerText=random;
+    let div = document.querySelector("div");
+    div.style.backgroundColor = random;
+    console.log("Colour Updated");
+});
+
+function GetRandomColour() {
+    let red = Math.floor(Math.random*255);
+    let green = Math.floor(Math.random*255);
+    let blue = Math.floor(Math.random*255);
+
+    let colour=`rgb(${red},${green},${blue})`;
+    return colour;
+
+}
