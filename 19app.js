eaa205c10d244170477edfff76f3bbb0ed941c42
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-22 09:20:28 +0530

    Add files via upload

---
+++ 19app.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("#btn");
+let ul=document.querySelector("ul");
+let inp=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+  let item=document.createElement("li");
+  item.innerText = inp.value;
+
+  let deltbtn=document.createElement("delete");
+  deltbtn.innerText = "delete";
+  deltbtn.classList.add("delete");
+
+item.appendChild(deltbtn);
+  ul.appendChild(item);
+    inp.value ="";
+});
+
+ul.addEventListener("click",function(event){
+  console.dir(event.target.nodeName);
+  console.log("button clickwd")
+});
