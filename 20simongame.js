b1c7ed40ce55564aff70f4b8d2680b04b9774a1e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-26 09:29:25 +0530

    Update 20simongame.js

--- 20simongame.js
+++ 20simongame.js
@@ -5,7 +5,7 @@ let btns=["yellow","red","purple","green"];
 let started=false;
 let level = 0;
 let h2=document.querySelector("h2");
-document.addEventListener("keypress" , function(){
+document.addEventListener("keypress" , function(){ 
 if(started==false){
     console.log("game started");
     started = true;
