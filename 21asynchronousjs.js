fee2b17dc4a0065affec2801f87099fcd0e376af
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-30 08:22:30 +0530

    Add files via upload

---
+++ 21asynchronousjs.js
@@ -0,0 +1,68 @@
+// h1=document.querySelector("h1");
+// h1.style.color="red";
+
+// // setTimeout(() =>{
+// //     h1.style.color="green";
+// // },1000);
+
+// // setTimeout(() =>{
+// //     h1.style.color="yellow";
+// // },2000);
+
+// function changecolor(color,delay,nextchangecolor){
+//     setTimeout(()=>{
+//         h1.style.color=color;
+//         if(nextchangecolor) nextchangecolor();
+//     },delay);
+//     }
+
+// changecolor("yellow",1000,()=>{
+//     changecolor("orange",1000,()=>{
+//         changecolor("green",1000);
+//     });
+// });
+
+function savetoDB(data){
+    return new Promise((success, failure) => {
+    let internetspeed = Math.floor(Math.random() *10)+1;
+    if(internetspeed>4){
+   success("success:data is save");
+    }else{
+     failure("failure:data is not saved");
+    }
+ });
+}
+// let request = savetoDB("apna college");
+// request.then(()=>{
+//     console.log("promise was resolve");
+//     console.log(request);
+// })
+// .catch(()=>{
+//     console.log("promise was rejected");
+//     console.log(request);
+// });
+// savetoDB("apna college")
+// .then(()=>{
+//     console.log("promise was resolve");
+   
+// })
+// .catch(()=>{
+//     console.log("promise was rejected");
+    
+// })
+
+savetoDB("apna college")
+.then(()=>{
+    console.log("data1 saved.");
+   return savetoDB("hellow");
+})
+.then(()=>{
+    console.log("data2 saved.");
+    return savetoDB("hellow");
+})
+.then(()=>{
+    console.log("data3 saved.");
+})
+.catch(()=>{
+    console.log("promise was rejected"); 
+});
